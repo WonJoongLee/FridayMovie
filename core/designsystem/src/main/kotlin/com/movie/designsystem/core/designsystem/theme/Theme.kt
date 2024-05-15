d511fa08405dcb59895e1d93c193839955587a30
isaac <realjoong@gmail.com>
2024-05-15 21:55:42 +0900

    change navigation bar color

--- core/designsystem/src/main/kotlin/com/movie/designsystem/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/movie/designsystem/core/designsystem/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.movie.fridaymovie.core.designsystem.R as DesignSystemR
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -43,6 +44,7 @@ fun FridayMovieTheme(
     if (!view.isInEditMode) {
         val window = (view.context as Activity).window
         window.statusBarColor = Color.Transparent.toArgb()
+        window.navigationBarColor = view.context.getColor(DesignSystemR.color.gray800_alpha50)
         WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
 
