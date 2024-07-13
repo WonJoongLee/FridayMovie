3949301b9a518f9b631429396d8a61556f48b4ab
isaac <realjoong@gmail.com>
2024-07-13 20:50:28 +0900

    remove logging

--- feature/detail/src/main/kotlin/com/movie/detail/navigation/MovieDetailNavigation.kt
+++ feature/detail/src/main/kotlin/com/movie/detail/navigation/MovieDetailNavigation.kt
@@ -1,6 +1,5 @@
 package com.movie.detail.navigation
 
-import android.util.Log
 import androidx.navigation.NavController
 import com.movie.navigation.FridayMovieScreens
 
@@ -9,13 +8,6 @@ fun NavController.navigateToMovieDetail(
     posterImageUrl: String,
     movieTitle: String,
 ) {
-    Log.e("@@@", "navigateToDetail, movieId: $movieId, posterImageUrl: $posterImageUrl, movieTitle: $movieTitle")
-    val temp = FridayMovieScreens.Detail.createRoute(
-        movieId = movieId,
-        posterImageUrl = posterImageUrl,
-        movieTitle = movieTitle,
-    )
-    Log.e("@@@", "navigateToDetail route : $temp")
     navigate(
         FridayMovieScreens.Detail.createRoute(
             movieId = movieId,
