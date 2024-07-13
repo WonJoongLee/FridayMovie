c6ad66b9b700cea6f7eec7997cc314acc76c8469
isaac <realjoong@gmail.com>
2024-07-13 21:12:07 +0900

    fix screenshot test

--- feature/home/src/screenshotTest/kotlin/com/movie/home/PopularMovieItemScreenShots.kt
+++ feature/home/src/screenshotTest/kotlin/com/movie/home/PopularMovieItemScreenShots.kt
@@ -1,22 +1,26 @@
 package com.movie.home
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.movie.designsystem.core.designsystem.theme.FridayMovieTheme
-import com.movie.domain.domain.Movie
+import com.movie.domain.domain.PopularMovie
 import com.movie.home.ui.PopularMovieItem
 
 class PopularMovieItemScreenShots {
 
+    @OptIn(ExperimentalSharedTransitionApi::class)
     @Preview(
         showBackground = true
     )
     @Composable
     fun PopularMovieItemScreenShotPreview() {
-        val samplePopularMovie = Movie(
+        val samplePopularMovie = PopularMovie(
             id = 1,
             title = "Joker: Folie à Deux (2024)",
             originalTitle = "Joker: Folie à Deux (2024)",
@@ -28,10 +32,19 @@ class PopularMovieItemScreenShots {
             backDropImageUrl = ""
         )
         FridayMovieTheme {
-            PopularMovieItem(
-                modifier = Modifier.width(100.dp),
-                movie = samplePopularMovie
-            )
+            SharedTransitionLayout {
+                AnimatedContent(
+                    targetState = 100,
+                    label = ""
+                ) { _ ->
+                    PopularMovieItem(
+                        modifier = Modifier.width(100.dp),
+                        animatedVisibilityScope = this,
+                        movie = samplePopularMovie,
+                        onClick = { _, _, _ -> }
+                    )
+                }
+            }
         }
     }
 }
