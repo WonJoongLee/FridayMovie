f99666cac0fcb771ba6b2356c10864b873b2b1b6
isaac <realjoong@gmail.com>
2024-07-13 22:17:49 +0900

    apply top app bar scroll behavior

--- feature/home/src/main/kotlin/com/movie/home/ui/HomeScreen.kt
+++ feature/home/src/main/kotlin/com/movie/home/ui/HomeScreen.kt
@@ -3,21 +3,24 @@ package com.movie.home.ui
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -29,7 +32,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.movie.fridaymovie.core.strings.R
 import com.movie.fridaymovie.core.designsystem.R as DesignSystemR
 
-@OptIn(ExperimentalSharedTransitionApi::class)
+@OptIn(ExperimentalSharedTransitionApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun SharedTransitionScope.HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
@@ -41,34 +44,37 @@ fun SharedTransitionScope.HomeScreen(
     ) -> Unit,
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                colorResource(id = DesignSystemR.color.gray800),
-                                colorResource(id = DesignSystemR.color.transparent)
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                colors = TopAppBarDefaults.topAppBarColors().copy(
+                    containerColor = colorResource(id = DesignSystemR.color.gray800),
+                    scrolledContainerColor = colorResource(id = DesignSystemR.color.gray800)
+                ),
+                title = {
+                    Text(
+                        modifier = Modifier
+                            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
+                        text = stringResource(id = R.string.main_top_app_name),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 28.sp,
+                        style = TextStyle(
+                            brush = Brush.horizontalGradient(
+                                colors = listOf(
+                                    colorResource(id = DesignSystemR.color.yellow900),
+                                    colorResource(id = DesignSystemR.color.yellow500)
+                                ),
                             )
                         )
                     )
-                    .statusBarsPadding()
-                    .padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
-                text = stringResource(id = R.string.main_top_app_name),
-                fontWeight = FontWeight.Bold,
-                fontSize = 28.sp,
-                style = TextStyle(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            colorResource(id = DesignSystemR.color.yellow900),
-                            colorResource(id = DesignSystemR.color.yellow500)
-                        ),
-                    )
-                )
+                },
+                scrollBehavior = scrollBehavior
             )
         },
         containerColor = colorResource(id = DesignSystemR.color.gray800)
