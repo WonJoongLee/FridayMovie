b4f1a98e4f565e6b1b77b6ceee74bdeafe3aa35a
isaac <realjoong@gmail.com>
2025-01-25 12:34:12 -0500

    remove useless code

--- feature/home/src/main/kotlin/com/movie/home/navigation/HomeNavigation.kt
+++
@@ -1,9 +0,0 @@
-package com.movie.home.navigation
-
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
-import com.movie.home.ui.HomeScreen
-import com.movie.navigation.FridayMovieScreens
-
-fun NavController.navigateToHome() = navigate(FridayMovieScreens.Home.route)
